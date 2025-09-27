952de1bc22c599aa819c75a4c819339897c1072e
gracelyyy <Gracely166@gmail.com>
2025-09-27 22:56:38 +0200

    Scripture.cs

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(' ').Select(w => new Word(w)).ToList();
+    }
+
+    public void HideRandomWords(int count)
+    {
+        Random rand = new Random();
+        var visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+
+        for (int i = 0; i < count && visibleWords.Count > 0; i++)
+        {
+            int index = rand.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public bool AllWordsHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+
+    public string GetDisplayText()
+    {
+        return $"{_reference.GetDisplayText()}\n" +
+               string.Join(" ", _words.Select(w => w.GetDisplayText()));
+    }
+}
