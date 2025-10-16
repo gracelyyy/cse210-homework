acaf368a6343841ad5a33e2d99009f572670720c
gracelyyy <Gracely166@gmail.com>
2025-10-16 19:03:57 +0200

    Program.cs
    Updated the main program to include personalized messages and a list of activities with summaries.

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ExerciseTracking
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mosa Molapo Personalized Exercise Tracker");
+            Console.WriteLine("----------------------------------------\n");
+
+            List<Activity> activities = new List<Activity>()
+            {
+                new Running("16 Oct 2025", 30, 5.0),   // 5 km run
+                new Cycling("16 Oct 2025", 45, 22.0),  // 22 kph cycling
+                new Swimming("16 Oct 2025", 25, 40)    // 40 laps swimming
+            };
+
+            foreach (Activity activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+
+            Console.WriteLine("\nKeep pushing, Mosa! 💪");
+        }
     }
 }
